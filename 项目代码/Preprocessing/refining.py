57676ed3dd4c33562dca092345d827d30186e843
zhangbingchen9 <987223692@qq.com>
2019-11-05 09:25:42 +0800

    updata

---
+++ 项目代码/Preprocessing/refining.py
@@ -0,0 +1,84 @@
+import cv2
+
+
+def VThin(image, array):
+    '''
+    :param image:二值化处理好的图像
+    :param array:映射表
+    :return:
+    '''
+    h, w = image.shape
+    NEXT = 1
+    for i in range(h):
+        for j in range(w):
+            if NEXT == 0:
+                NEXT = 1
+            else:
+                M = int(image[i, j - 1]) + int(image[i, j]) + int(image[i, j + 1]) if 0 < j < w - 1 else 1
+                if image[i, j] == 0 and M != 0:
+                    a = [0] * 9
+                    for k in range(3):
+                        for l in range(3):
+                            if -1 < (i - 1 + k) < h and -1 < (j - 1 + l) < w and image[i - 1 + k, j - 1 + l] == 255:
+                                a[k * 3 + l] = 1
+                    sum = a[0] * 1 + a[1] * 2 + a[2] * 4 + a[3] * 8 + a[5] * 16 + a[6] * 32 + a[7] * 64 + a[8] * 128
+                    image[i, j] = array[sum] * 255
+                    if array[sum] == 1:
+                        NEXT = 0
+    return image
+
+
+def HThin(image, array):
+    '''
+    :param image:二值化之后的图像
+    :param array:映射表
+    :return:
+    '''
+    h, w = image.shape
+    NEXT = 1
+    for j in range(w):
+        for i in range(h):
+            if NEXT == 0:
+                NEXT = 1
+            else:
+                M = int(image[i - 1, j]) + int(image[i, j]) + int(image[i + 1, j]) if 0 < i < h - 1 else 1
+                if image[i, j] == 0 and M != 0:
+                    a = [0] * 9
+                    for k in range(3):
+                        for l in range(3):
+                            if -1 < (i - 1 + k) < h and -1 < (j - 1 + l) < w and image[i - 1 + k, j - 1 + l] == 255:
+                                a[k * 3 + l] = 1
+                    sum = a[0] * 1 + a[1] * 2 + a[2] * 4 + a[3] * 8 + a[5] * 16 + a[6] * 32 + a[7] * 64 + a[8] * 128
+                    image[i, j] = array[sum] * 255
+                    if array[sum] == 1:
+                        NEXT = 0
+    return image
+
+
+def refining(image):
+    '''
+    细化函数
+    :param image:已完成二值化的图像
+    :return:细化处理好的图像
+    '''
+    # 映射表
+    array = [0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, \
+             1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, \
+             0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, \
+             1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, \
+             1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+             1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, \
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+             0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, \
+             1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, \
+             0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, \
+             1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, \
+             1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+             1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, \
+             1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, \
+             1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0]
+    for i in range(10):
+        VThin(image, array)
+        HThin(image, array)
+    return image
